10a7f10daf69b94c8b6f8ecf957b5c93f489b987
SuperCoder <supercoder@AirvonSerCoder3.fritz.box>
2022-02-09 20:33:12 +0100

    first com

---
+++ assets/js/main.js
@@ -0,0 +1,69 @@
+//Lev1_14//
+console.log(Boolean(100));
+console.log(Boolean(3.14));
+console.log(Boolean(-15));
+console.log(Boolean("hello"));
+console.log(Boolean("false"));
+console.log(Boolean(1 + 7 + 3.14));
+console.log(Boolean(0));
+console.log(Boolean(-0));
+console.log(Boolean(NaN));
+console.log(Boolean(null));
+console.log(Boolean(false));
+
+//Lev1_15//
+let num = 5;
+let anotherNum = 8;
+
+console.log(num == anotherNum);
+console.log(num > anotherNum);
+console.log(num < anotherNum);
+console.log(num >= anotherNum);
+console.log(num <= anotherNum);
+
+console.log(num == "5");
+console.log(num != 8);
+console.log(num != "5");
+
+//Lev1_8//
+let x = 20;
+let y = 30;
+
+console.log(x + y);
+console.log(x - y);
+console.log(y - x);
+console.log(x * y);
+console.log(x / y);
+
+let z = 10;
+let resultOne = x * y / z;
+console.log(resultOne);
+
+//2//
+let a = 15;
+let b = 9;
+
+console.log(15 % 9);
+
+let c = 20;
+let resultTwo = a + b * c;
+console.log(resultTwo);
+
+a++;
+console.log(a);
+
+b--;
+console.log(b);
+
+let resultThree = a - b;
+console.log(resultThree);
+console.log(resultOne % resultTwo);
+
+//Lev1_4//
+let myName = "John";
+console.log(myName);
+let pi = 3.14;
+console.log(pi);
+let n = "NaN";
+console.log(n);
+let f = "false";
